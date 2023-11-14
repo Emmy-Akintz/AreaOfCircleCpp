0a151df1faddc93d98707a13ac1974b8e7ff9b7e
Ayomide Akintan <emzyakints2005@gmail.com>
2023-11-14 01:23:21 +0100

    Update

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+//using namespace std;
+
+float areaOfCircle(float radius, float pi);
+
+int main()
+{
+    const float pi = 3.142;
+    float radius;
+    std::cout << "\n Enter radius: ";
+    std::cin >> radius;
+    float area = areaOfCircle(radius, pi);
+    std::cout << "\n The area is " << area;
+    return 0;
+}
+
+float areaOfCircle(float radius, float pi)
+{
+    return pi * radius * radius;
+}
